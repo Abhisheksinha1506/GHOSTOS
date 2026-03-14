58c1191c7563f4dc3faed7596b854e84ac330556
Abhishek Sinha <abhisheksinha1506@gmail.com>
2026-03-14 12:37:05 +0530

    fix: fetch env keys from Vercel serverless endpoint on boot

--- api/index.js
+++ api/index.js
@@ -1,24 +1,8 @@
-const fs = require('fs');
-const path = require('path');
-
 module.exports = (req, res) => {
-  const htmlPath = path.join(__dirname, '..', 'index.html');
-  let html = fs.readFileSync(htmlPath, 'utf8');
-
-  // Pick up Vercel environment variables
-  const envVars = {
+  res.setHeader('Content-Type', 'application/json');
+  res.setHeader('Cache-Control', 'no-store');
+  res.status(200).json({
     GROQ_API_KEY: process.env.GROQ_API_KEY || '',
     OPENROUTER_API_KEY: process.env.OPENROUTER_API_KEY || ''
-  };
-
-  // Inject them as window.ENV before </head>
-  const envScript = `
-    <script>
-      window.ENV = ${JSON.stringify(envVars)};
-    </script>
-  `;
-  html = html.replace('</head>', envScript + '</head>');
-
-  res.setHeader('Content-Type', 'text/html');
-  res.status(200).send(html);
+  });
 };
